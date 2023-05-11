0db6cbff35f95d010e38cef145785b5cb79514a8
irhamasyabani <129582203+irhamasyabani@users.noreply.github.com>
2023-05-11 11:40:14 +0700

    Update AnalisisGravimetri.py

--- AnalisisGravimetri.py
+++ AnalisisGravimetri.py
@@ -122,4 +122,3 @@ if (selected == "Perhitungan Kadar Abu Sampel") :
     if st.button('Hitung') :
         kadar_abu = (massa_sebelum - massa_setelah) / (massa_sampel * 100)
         st.success(f'Hasil kadar abu adalah {kadar_abu}%')
-	st.balloons()
